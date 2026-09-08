23947f9982f2a2838b00bc43fda3e5303d639343
Yura_Kirpichnikov <Georgestallker@gmail.com>
2026-09-08 16:07:50 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 
-// Replace "goit-js-hw-11" below with your actual repository name
-// if you rename the repo, so GitHub Pages can resolve asset paths.
 export default defineConfig({
-  base: '/goit-js-hw-11/',
+  root: 'src',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 });
